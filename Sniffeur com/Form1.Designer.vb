099666ed8f9a112360d66d9e02409ebafef2bd0a
ghost54770 <ghost54.lol@hotmail.fr>
2021-11-21 21:54:33 +0100

    Init

---
+++ Sniffeur com/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox_FenetrePrincipale = New System.Windows.Forms.TextBox()
+        Me.ComboBox_PortCOM = New System.Windows.Forms.ComboBox()
+        Me.Label_PortCOM = New System.Windows.Forms.Label()
+        Me.Label_Baud = New System.Windows.Forms.Label()
+        Me.ComboBox_Baud = New System.Windows.Forms.ComboBox()
+        Me.Label_NombreBitsDonnée = New System.Windows.Forms.Label()
+        Me.ComboBox_NombreBitsDonnee = New System.Windows.Forms.ComboBox()
+        Me.Label_NombreBitsStop = New System.Windows.Forms.Label()
+        Me.ComboBox_NombreBitsStop = New System.Windows.Forms.ComboBox()
+        Me.Label_Parite = New System.Windows.Forms.Label()
+        Me.ComboBox_Parite = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'TextBox_FenetrePrincipale
+        '
+        Me.TextBox_FenetrePrincipale.Location = New System.Drawing.Point(403, 12)
+        Me.TextBox_FenetrePrincipale.Multiline = True
+        Me.TextBox_FenetrePrincipale.Name = "TextBox_FenetrePrincipale"
+        Me.TextBox_FenetrePrincipale.Size = New System.Drawing.Size(776, 476)
+        Me.TextBox_FenetrePrincipale.TabIndex = 0
+        '
+        'ComboBox_PortCOM
+        '
+        Me.ComboBox_PortCOM.FormattingEnabled = True
+        Me.ComboBox_PortCOM.Location = New System.Drawing.Point(96, 26)
+        Me.ComboBox_PortCOM.Name = "ComboBox_PortCOM"
+        Me.ComboBox_PortCOM.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_PortCOM.TabIndex = 1
+        '
+        'Label_PortCOM
+        '
+        Me.Label_PortCOM.AutoSize = True
+        Me.Label_PortCOM.Location = New System.Drawing.Point(12, 29)
+        Me.Label_PortCOM.Name = "Label_PortCOM"
+        Me.Label_PortCOM.Size = New System.Drawing.Size(55, 13)
+        Me.Label_PortCOM.TabIndex = 2
+        Me.Label_PortCOM.Text = "N° de port"
+        '
+        'Label_Baud
+        '
+        Me.Label_Baud.AutoSize = True
+        Me.Label_Baud.Location = New System.Drawing.Point(12, 56)
+        Me.Label_Baud.Name = "Label_Baud"
+        Me.Label_Baud.Size = New System.Drawing.Size(32, 13)
+        Me.Label_Baud.TabIndex = 4
+        Me.Label_Baud.Text = "Baud"
+        '
+        'ComboBox_Baud
+        '
+        Me.ComboBox_Baud.FormattingEnabled = True
+        Me.ComboBox_Baud.Location = New System.Drawing.Point(96, 53)
+        Me.ComboBox_Baud.Name = "ComboBox_Baud"
+        Me.ComboBox_Baud.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_Baud.TabIndex = 3
+        '
+        'Label_NombreBitsDonnée
+        '
+        Me.Label_NombreBitsDonnée.AutoSize = True
+        Me.Label_NombreBitsDonnée.Location = New System.Drawing.Point(12, 110)
+        Me.Label_NombreBitsDonnée.Name = "Label_NombreBitsDonnée"
+        Me.Label_NombreBitsDonnée.Size = New System.Drawing.Size(78, 13)
+        Me.Label_NombreBitsDonnée.TabIndex = 6
+        Me.Label_NombreBitsDonnée.Text = "Bits de donnée"
+        '
+        'ComboBox_NombreBitsDonnee
+        '
+        Me.ComboBox_NombreBitsDonnee.FormattingEnabled = True
+        Me.ComboBox_NombreBitsDonnee.Location = New System.Drawing.Point(96, 107)
+        Me.ComboBox_NombreBitsDonnee.Name = "ComboBox_NombreBitsDonnee"
+        Me.ComboBox_NombreBitsDonnee.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_NombreBitsDonnee.TabIndex = 5
+        '
+        'Label_NombreBitsStop
+        '
+        Me.Label_NombreBitsStop.AutoSize = True
+        Me.Label_NombreBitsStop.Location = New System.Drawing.Point(12, 137)
+        Me.Label_NombreBitsStop.Name = "Label_NombreBitsStop"
+        Me.Label_NombreBitsStop.Size = New System.Drawing.Size(62, 13)
+        Me.Label_NombreBitsStop.TabIndex = 8
+        Me.Label_NombreBitsStop.Text = "Bits de stop"
+        '
+        'ComboBox_NombreBitsStop
+        '
+        Me.ComboBox_NombreBitsStop.FormattingEnabled = True
+        Me.ComboBox_NombreBitsStop.Location = New System.Drawing.Point(96, 134)
+        Me.ComboBox_NombreBitsStop.Name = "ComboBox_NombreBitsStop"
+        Me.ComboBox_NombreBitsStop.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_NombreBitsStop.TabIndex = 7
+        '
+        'Label_Parite
+        '
+        Me.Label_Parite.AutoSize = True
+        Me.Label_Parite.Location = New System.Drawing.Point(12, 83)
+        Me.Label_Parite.Name = "Label_Parite"
+        Me.Label_Parite.Size = New System.Drawing.Size(78, 13)
+        Me.Label_Parite.TabIndex = 10
+        Me.Label_Parite.Text = "Bits de donnée"
+        '
+        'ComboBox_Parite
+        '
+        Me.ComboBox_Parite.FormattingEnabled = True
+        Me.ComboBox_Parite.Location = New System.Drawing.Point(96, 80)
+        Me.ComboBox_Parite.Name = "ComboBox_Parite"
+        Me.ComboBox_Parite.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox_Parite.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1191, 500)
+        Me.Controls.Add(Me.Label_Parite)
+        Me.Controls.Add(Me.ComboBox_Parite)
+        Me.Controls.Add(Me.Label_NombreBitsStop)
+        Me.Controls.Add(Me.ComboBox_NombreBitsStop)
+        Me.Controls.Add(Me.Label_NombreBitsDonnée)
+        Me.Controls.Add(Me.ComboBox_NombreBitsDonnee)
+        Me.Controls.Add(Me.Label_Baud)
+        Me.Controls.Add(Me.ComboBox_Baud)
+        Me.Controls.Add(Me.Label_PortCOM)
+        Me.Controls.Add(Me.ComboBox_PortCOM)
+        Me.Controls.Add(Me.TextBox_FenetrePrincipale)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox_FenetrePrincipale As TextBox
+    Friend WithEvents ComboBox_PortCOM As ComboBox
+    Friend WithEvents Label_PortCOM As Label
+    Friend WithEvents Label_Baud As Label
+    Friend WithEvents ComboBox_Baud As ComboBox
+    Friend WithEvents Label_NombreBitsDonnée As Label
+    Friend WithEvents ComboBox_NombreBitsDonnee As ComboBox
+    Friend WithEvents Label_NombreBitsStop As Label
+    Friend WithEvents ComboBox_NombreBitsStop As ComboBox
+    Friend WithEvents Label_Parite As Label
+    Friend WithEvents ComboBox_Parite As ComboBox
+End Class
---
+++ Sniffeur com/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System
+Imports System.IO.Ports
+Imports System.Threading
+
+Public Class Form1
+
+    'Chargement des taches de fond
+    Private Sub Form1_Chargement(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Remplissage du combobox "ComboBox_PortCOM"
+        For Each port As String In My.Computer.Ports.SerialPortNames
+            ComboBox_PortCOM.Items.Add(port)
+        Next
+
+        'Remplissage du combobox "ComboBox_Baud"
+        Dim baud = New Integer() {110, 220, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200}
+        For i As Integer = 0 To baud.Length - 1 Step 1
+            ComboBox_Baud.Items.Add(baud(i))
+        Next
+        'Remplissage du combobox "ComboBox_Parité"
+        Dim parité = New Integer() {0, 1, 2}
+        For i As Integer = 0 To parité.Length - 1 Step 1
+            ComboBox_Parite.Items.Add(parité(i))
+        Next
+
+        'Remplissage du combobox "ComboBox_NombreBitsDonnée"
+        Dim bitsDonnee = New Integer() {4, 5, 6, 7, 8}
+        For i As Integer = 0 To bitsDonnee.Length - 1 Step 1
+            ComboBox_NombreBitsDonnee.Items.Add(bitsDonnee(i))
+        Next
+
+        'Remplissage du combobox "ComboBox_NombreBitsStop"
+        Dim bitsStop = New Integer() {0, 1, 2}
+        For i As Integer = 0 To bitsStop.Length - 1 Step 1
+            ComboBox_NombreBitsStop.Items.Add(bitsStop(i))
+        Next
+
+    End Sub
+
+    'Private Sub ComboBox_Remplissage(sender As System.Object, e As EventArgs) Handles ComboBox_PortCOM.Click
+    '    ComboBox_PortCOM.Items.Clear()
+    '    ObtenirListePortCOM()
+
+    'End Sub
+    'Sub Main()
+    '    '  ObtenirListePortCOM()
+    '    ComboBox_PortCOM.Items.Add("1")
+
+    '    For compteur As Integer = 1 To 5 Step 1
+    '        ComboBox_PortCOM.Items.Add("1")
+
+    '    Next
+    'End Sub
+
+
+
+    Private Sub ObtenirListePortCOM()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label_PortCOM.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label_Baud.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox_Baud.SelectedIndexChanged
+
+    End Sub
+End Class
+
+
